cdd76d5627ca473dcc9e3254d11b519fe846eafb
AldiMuliaRismanto <aldimuliarismanto27@gmail.com>
2023-07-23 15:26:28 +0700

    add template

---
+++ resources/views/admin/v_dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.v_template')
+
+@section('title','Dashboard|Laundry Aja')
+    
+
+@section('content')
+
+<h1>Laundry Aja</h1>
+    
+@endsection
---
+++ resources/views/layout/v_nav.blade.php
@@ -0,0 +1,46 @@
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+        
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+           <li class="nav-header">MAIN NAVIGATION </li>
+      <li class ="nav-item">
+        <a href="/" class="nav-link">
+          <i class="nav-icon fas fa-home"></i>
+          <p>
+            Dashboard
+          </p>
+        </a>
+          <li class="nav-item">
+            <a href="/Dosen" class="nav-link">
+              <i class="fas fa-book nav-icon"></i>
+              <p>Jenis Laundry</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/User" class="nav-link">
+              <i class="fas fa-user nav-icon"></i>
+              <p>Transaksi</p>
+            </a>
+          </li>
+        <li class="nav-header">Data </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class=""></i>
+          <p>
+            Data Karyawan
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-user nav-icon"></i>
+              <p>Data Kasir</p>
+            </a>
+          </li>
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
+</div>
