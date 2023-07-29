c26746d65d9a572244f50d05f951c1f3c4b61177
zura12 <zura.permata123@gmail.com>
2023-07-30 01:00:55 +0700

    menambahkan customer dan layanan

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,11 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    
     use HasFactory;
+
 
     protected $fillable = [
         'name',
         'address',
         'telephone',
     ];
+
+    
 }
